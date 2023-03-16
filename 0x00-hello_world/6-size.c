e69454676d3fd8ae3a1f7d6b7ef30bf2fd248495
LAKRAD SAFAA <mlle.safaalakrad@gmail.com>
2023-03-16 13:07:11 -0700

    hello_word

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* You should produce the exact same output as in the example
+* Warnings are allowed
+*
+* Your program should return 0
+*Â/
+int main(void)
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+	printf("size of a char: %1d byte(s)\n", sizeof(c));
+	printf("size of a int: %1d byte(s)\n", sizeof(i));
+	printf("size of a long: %1d byte(s)\n", sizeof(li));
+	printf("size of a long long: %1d byte(s)\n", sizeof(lli));
+	printf("size of a float: %1d byte(s)\n", sizeof(f))
+	return(0);
+}
