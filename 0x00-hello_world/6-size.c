5facce106e8853c93bfed44ada0624164a93fbb9
LAKRAD SAFAA <mlle.safaalakrad@gmail.com>
2023-03-16 13:14:50 -0700

    hello_word

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,17 +5,19 @@
 * Warnings are allowed
 *
 * Your program should return 0
-*Â/
+*/
 int main(void)
+{
 	char c;
 	int i;
-	long li;
+	long li;     
 	long long lli;
 	float f;
+
 	printf("size of a char: %1d byte(s)\n", sizeof(c));
-	printf("size of a int: %1d byte(s)\n", sizeof(i));
+	printf("size of a int: %1d byte(s)\n", sizeof(i)); 
 	printf("size of a long: %1d byte(s)\n", sizeof(li));
 	printf("size of a long long: %1d byte(s)\n", sizeof(lli));
-	printf("size of a float: %1d byte(s)\n", sizeof(f))
-	return(0);
+	printf("size of a float: %1d byte(s)\n", sizeof(f);)
+	rturn(0);
 }
