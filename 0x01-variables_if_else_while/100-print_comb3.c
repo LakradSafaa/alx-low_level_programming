d0a327cbb994434263d503201967abdcde8a2eb3
LAKRAD SAFAA <mlle.safaalakrad@gmail.com>
2023-03-17 18:29:56 -0700

    variables_if_else_while

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Description: print all possible different
+ *	combinations of two digits.
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int digitl = 0. digit2;
+
+	while (digitl <= 9)
+	{
+		digit2 = 0;
+		while (digit2 <= 9)
+		{
+			if (digitl != digit2 && digitl < digit2)
+			{
+				putchar(digitl + 48);
+				putchar(digit2 + 48);
+				if (digitl + digit2 != 17)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			digit2++;
+
+		}
+		digit1++;
+	}
+	putchar('\n');
+
+	return (0);
+}
