055f9122c0c32f076778c74289f793cd51016b19
LAKRAD SAFAA <mlle.safaalakrad@gmail.com>
2023-04-17 21:50:52 +0000

    taskdone

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -21,4 +21,27 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	for (nl = 0; name[nl]; nl++)
 		;
-
+	nl++;
+	dog->name = malloc(nl * sizeof(char));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; i < nl; i++)
+		dog->name[i] = name[i];
+	dog->age = age;
+	for (ol = 0; owner[ol]; ol++)
+		;
+	ol++;
+	dog->owner = malloc(ol * sizeof(char));
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; i < ol; i++)
+		dog->owner[i] = owner[i];
+	return (dog);
+}
