5b0dab020e24d2caefd57e143a92fcc8526a7736
Lakrad Safaa <mlle.safaalakrad@gmail.com>
2023-05-01 11:57:41 -0700

    taskdone

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ * @head: head of a list.
+ *
+ * Return: no return.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp. *node;
+
+	if (!head)
+	{
+		node = *head;
+		while ((temp = curr) != NULL)
+		{
+			tmp = node
+			node = node->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
