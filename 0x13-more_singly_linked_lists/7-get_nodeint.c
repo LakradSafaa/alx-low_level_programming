8cef51b4d1cc87b18b622b1195a7339491484669
Lakrad Safaa <mlle.safaalakrad@gmail.com>
2023-05-01 13:42:07 -0700

    taskdone

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int n;
 	listint_t *node;
 
-	for (node = head, n = 0; node && n < index; node = node->next, n++); 
+	for (node = head, n = 0; node && n < index; node = node->next, n++)
+		;
 
 	return (node);
 }
